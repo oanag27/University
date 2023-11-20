869bdfe7bdcff43beb8814d3846d9f71b6385a18
Gog-Oana <115409789+oanag27@users.noreply.github.com>
2023-11-20 12:38:50 +0200

    refactor

--- Semester3/advanced programming methods/lab5/src/model/statement/IStmt.java
+++
@@ -1,8 +0,0 @@
-package model.statement;
-
-import model.exception.MyException;
-
-public interface IStmt {
-    ProgramState execute(ProgramState state) throws MyException;
-    IStmt deepCopy();
-}
